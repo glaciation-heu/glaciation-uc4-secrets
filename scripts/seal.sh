a57ee7c2622de2da82df63fa31fa2544b30b8a89
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2025-05-20 12:42:16 +0200

    Automatic output path

--- scripts/seal.sh
+++ scripts/seal.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-if [ $# -ne 2 ]; then
-    echo "Usage: seal.sh <template_file> <output_file>" >&2
+if [ $# -ne 1 ]; then
+    echo "Usage: seal.sh <template_file>" >&2
     exit 1
 fi
 
-TEMPLATE_FILE="$1"
-OUTPUT_FILE="$2"
 source .env
+TEMPLATE_FILE="$1"
+TEMPLATE_FILE_NAME="$(basename "$1")"
+OUTPUT_FILE="$OUTPUT_DIR/$TEMPLATE_FILE_NAME"
+touch "$OUTPUT_FILE"
 ./scripts/render.py "$TEMPLATE_FILE" | kubeseal  --format yaml > "$OUTPUT_FILE"
