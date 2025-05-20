0f881ca90d461e950a27f8ed0f7c6f942f70511a
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2025-05-20 11:37:47 +0200

    Clean up render.py

--- scripts/render.py
+++ scripts/render.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 import sys
 import os
 import base64
@@ -9,28 +10,27 @@ def replace_base64(input: str) -> str:
     """Replaces tags <BASE64>...</BASE64> with base64 of its internals."""
     start_tag = "<BASE64>"
     end_tag = "</BASE64>"
-    while True:
-        start = input.find(start_tag)
-        if start == -1:
-            break
-        end = input.find(end_tag, start)
-        if end == -1:
-            raise Exception("Missing end tag </BASE64>")
-        content = input[start + len(start_tag):end]
-        content = "\r\n".join(l.strip() for l in content.splitlines() if l.strip() != "")
-        encoded = base64.b64encode(content.encode()).decode()
-        input = input[:start] + encoded + input[end + len(end_tag):]
-    return input
+    pattern = re.compile(f'{re.escape(start_tag)}(.*?){re.escape(end_tag)}', re.DOTALL)
+    return pattern.sub(_replace_base64_match, input)
 
 
-if len(sys.argv) != 2:
-    print("Usage: render.py <template_file>", file=sys.stderr)
-    sys.exit(1)
+def _replace_base64_match(match):
+    content = match.group(1)
+    content = "\r\n".join(l.strip() for l in content.splitlines() if l.strip() != "")
+    encoded = base64.b64encode(content.encode()).decode()
+    return f'{encoded}'
 
 
-with open(sys.argv[1]) as template_file:
-    template_text = str(template_file.read())
-    template = Template(template_text)
-    template_rendered = template.substitute(os.environ)
-    result = replace_base64(template_rendered)
-    print(result)
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: render.py <template_file>", file=sys.stderr)
+        sys.exit(1)
+    with open(sys.argv[1]) as template_file:
+        template_text = str(template_file.read())
+        template = Template(template_text)
+        template_rendered = template.substitute(os.environ)
+        result = replace_base64(template_rendered)
+        print(result)
+
+if __name__ == "__main__":
+    main()
